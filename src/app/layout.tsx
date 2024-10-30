6e1015c05937728ad23c0ff3df996d682c7f7ef6
Arman Alam <armanalam78578@gmail.com>
2024-10-31 04:17:28 +0530

    description added in meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,12 +8,13 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Arman Alam | Full Stack Web Developer",
-  description: " Expert in Next.js and React.js | MERN Technology | Node.js, Express.js, JavaScript & mongoDB",
+  description: "My experience includes working on front-end and back-end technologies, creating responsive web applications. I'm proficient in languages such as JavaScript, HTML, CSS, Next.js, React.js, Node.js, MongoDB, MySQL",
   keywords: ["Arman Alam", "Arman Alam Portfolio", "Arman Alam Motihari", "Mrarmanalam","armanalam_motihari", 
     "Arman Alam Web Developer", "Arman Alam Full Stack Developer", "Arman Alam React.js Developer", 
-    "Arman Alam Node.js Developer", "Experienced Web Developer", "Experienced Full Stack Web Developer", "Projects", "My Services", "Contact Us" ],
+    "Arman Alam Node.js Developer", "Experienced Web Developer", "Experienced Full Stack Web Developer", 
+    "Projects", "My Services", "Contact Us", "clone", "chat", "youtube", "experience" ],
   authors: { name: "Arman Alam", url: "https://github.com/Mr-armanalam" },
-  icons: "/armanhd3.png"
+  icons: "/armanhd3.png",
 };
 
 export default function RootLayout({
