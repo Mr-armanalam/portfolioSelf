3b6ee4ba5143ac447209d8589efa8a14188a1be4
Arman Alam <armanalam78578@gmail.com>
2024-10-30 20:20:46 +0530

    service hover now effectively work

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -11,7 +11,7 @@ const page = () => {
             <div key={i} className='border min-w-[22rem] md:min-w-96 max-w-md h-[17rem] flex-1 rounded-lg shadow-md hover:shadow-cstmclr-300 border-cstmclr-200 '>
               <div className='max-w-fit relative h-[17rem] trnstion'>
                 <Image className='h-[86%] rounded-t-lg' src={services[i]?.cardImage} alt='services' ></Image>
-                <div className='absolute p-8 bottom-0 left-0 -z-[1000] rounded-lg maketrnstion bg w-full bg-gray-100 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-5 '>
+                <div className='absolute p-8 bottom-0 left-0 -z-50 rounded-lg maketrnstion bg w-full bg-gray-100 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-5 '>
                   <h1 className='text-center text-cstmclr-50 pt-4 font-sans font-extrabold text-xl'>{services[i]?.serviceName} 
                     <p className='text-white pt-6 font-sans font-bold text-base'>{services[i]?.serviceDescription}</p>               
                     {/* <p className='bg-gradient-to-r from-white to-black bg-clip-text text-transparent pt-6 font-sans font-bold text-base'>{services[i]?.serviceDescription}</p>                */}
