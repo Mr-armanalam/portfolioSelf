850e57d78f9b75707e4deb13ebcd4febc3910bdf
Arman Alam <armanalam78578@gmail.com>
2024-11-18 19:08:18 +0530

    resume updated

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,9 +1,6 @@
 import { Metadata } from 'next';
 import React from 'react'
 
-// import resume from "./rersume.jpg"
-// import { FaDownload } from "react-icons/fa6";
-
 export const metadata: Metadata = {
   title: 'Arman Alam Resume',
   description: 'Welcome to my Resume page, a detailed snapshot of my professional journey and accomplishments.',
@@ -18,15 +15,6 @@ export const metadata: Metadata = {
 const page = () => {
   return (
      <main className="pt-16 relative border h-fit w-full justify-center flex">
-      
-      {/* <div className='text-cstmclr-700 absolute top-24 lg:right-40 md:right-12 right-4 xl:right-64 text-2xl'>
-        <a href="/static/resumepdf.pdf" download="Arman resume"> <FaDownload /></a>
-      </div> */}
-      {/* <img
-        className="self-center h-[100vh] md:w-[70vw] lg:w-[55vw] xl:w-[42vw]"
-        src={resume.src}
-      /> */}
-
       <embed 
         color='transparent' 
         className="self-center h-[82vh] md:h-[100vh] w-full" 
