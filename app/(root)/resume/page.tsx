8780ce0a645d632e2f89a716c69921a40175fc2f
Arman Alam <armanalam78578@gmail.com>
2025-01-10 22:05:25 +0530

    space added in resume title

--- app/(root)/resume/page.tsx
+++ app/(root)/resume/page.tsx
@@ -1,31 +1,31 @@
-import { Metadata } from 'next';
-import React from 'react'
+import { Metadata } from "next";
+import React from "react";
 
 export const metadata: Metadata = {
-  title: 'Arman Alam Resume',
-  description: 'Welcome to my Resume page, a detailed snapshot of my professional journey and accomplishments.',
+  title: "Arman Alam | Resume",
+  description:
+    "Welcome to my Resume page, a detailed snapshot of my professional journey and accomplishments.",
   keywords: ["Resume", "Arman's Resume", "Arman Alam Resume"],
   openGraph: {
     type: "website",
     url: "https://armanalam.vercel.app/resume",
     images: "/resume_page.png",
   },
-}
+};
 
 const page = () => {
   return (
-     <main className="pt-16 relative border h-fit w-full justify-center flex">
-      <embed 
-        color='transparent' 
-        className="self-center h-[82vh] md:h-[100vh] w-full" 
-        src="/static/resume.pdf" 
-        type="application/pdf" 
-        width={"100%"} 
+    <main className="relative flex h-fit w-full justify-center border pt-16">
+      <embed
+        color="transparent"
+        className="h-[82vh] w-full self-center md:h-[100vh]"
+        src="/static/resume.pdf"
+        type="application/pdf"
+        width={"100%"}
         height={"100%"}
       />
     </main>
   );
-}
+};
 
-export default page
-
+export default page;
